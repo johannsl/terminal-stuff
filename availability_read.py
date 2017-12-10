cb8b4e91976e8d1097d90d9ac45b6f8dec4f5187
johannsl <johannsl@stud.ntnu.no>
2017-12-10 05:00:09 +0100

    Code seems to work

--- availability_read.py
+++ availability_read.py
@@ -1,1 +1,26 @@
 # Read file and present output
+
+def main():
+    content = []
+    try:
+        status = open("status.txt", "r")
+        content = status.read().split("\n")
+        status.close()
+    except Exception as e:
+        print("status.txt read failure")
+        print(e)
+        return
+    
+    internet_availability = float(content[1]) / float(content[0]) * 100
+    if content[3] == "0":
+        server_availability = 100.0
+    else:
+        server_availability = float(content[4]) / float(content[3]) * 100
+
+    print("SERVER AVAILABILITY STATUS:")
+    print("Internet availability: " + str(internet_availability) + " %")
+    print("Server availability: " + str(server_availability) + " %")
+    print("\n\n")
+    return
+
+main()
--- availability_write.py
+++ availability_write.py
@@ -1,20 +1,106 @@
+import os
+import datetime
+
 # check internet
 # check uptime
-# write to file (every 6 hours?)
+# write to file (every 2 hours?)
+# statusfile structure:
+#   total pings
+#   accepted pings
+#   last runtime
+#   expected iterations
+#   actual iterations
+
+hostname = "8.8.8.8"
+frequency = 2 # hours
+time_buffer = 60 * 5 # seconds
 
 def main():
-    pingInternet()
-    checkUptime()
-    writeToFile()
+    connection, response = pingInternet()
+    uptime = checkUptime()
+    writeToFile(connection, response, uptime)
     return
 
 def pingInternet():
-    return
+    response = os.system("ping -c 1 " + hostname)
+    if response == 0:
+        return ["success", response]
+    else:
+        return ["failure", response]
 
 def checkUptime():
-    return
+    try:
+        uptime = open("/proc/uptime", "r")
+        uptime_seconds = float(uptime.readline().split()[0])
+        uptime.close()
+    except Exception as e:
+        print("checkUptime failure")
+        print(e)
+        return "failure"
+    return uptime_seconds
 
-def writeToFile():
+def writeToFile(connection, response, uptime):
+    current_time = datetime.datetime.now()
+    write_status = True
+    
+    if not os.path.isfile("status.txt"):
+        status = open("status.txt", "w")
+        content = [0, 0, current_time, 0, 0]
+        status.write(str(content[0]) + "\n")
+        status.write(str(content[1]) + "\n")
+        status.write(str(content[2]) + "\n")
+        status.write(str(content[3]) + "\n")
+        status.write(str(content[4]) + "\n")
+        status.write("\n\n")
+        status.close()
+
+    # Status file
+    new_content = []
+    try:
+        status = open("status.txt", "r+")
+        old_content = status.read().split("\n")
+
+        new_content.append(int(old_content[0]) + 1)
+        if connection == "success":
+            new_content.append(int(old_content[1]) + 1)
+        else:
+            new_content.append(int(old_content[1]))
+        new_content.append(str(current_time))
+        last_run = datetime.datetime.strptime(old_content[2], "%Y-%m-%d %H:%M:%S.%f")
+        delta_time = (current_time - last_run).total_seconds() + time_buffer
+        delta_hours = int(divmod(delta_time, 3600)[0])
+        expected_iterations = delta_hours / frequency
+        new_content.append(int(old_content[3]) + expected_iterations)
+        new_content.append(int(old_content[4]) + 1)
+        
+        status.seek(0)
+        status.write(str(new_content[0]) + "\n")
+        status.write(str(new_content[1]) + "\n")
+        status.write(str(new_content[2]) + "\n")
+        status.write(str(new_content[3]) + "\n")
+        status.write(str(new_content[4]) + "\n")
+        status.write("\n\n")
+        
+        status.close()
+    except Exception as e:
+        print("status.txt write failure")
+        print(e)
+        write_status = False
+
+    # Log file
+    try:
+        log = open("log.txt", "a")
+        log.write("current_time: " + str(current_time) + "\n")
+        log.write("connection: " + connection + "\n")
+        log.write("uptime: " + str(uptime) + "\n")
+        if write_status == False:
+            log.write("Write to status.txt failed \n")
+        log.write("\n\n")
+        log.close()
+    except Exception as e:
+        print("log.txt write failure")
+        print(e)
+        write_status = False
     return
     
 main()
